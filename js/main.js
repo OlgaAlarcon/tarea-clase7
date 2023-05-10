f4d4157da355929f078da4811aa6062d193aa7ec
OlgaAlarcon <olguitaalar25@gmail.com>
2023-05-09 21:29:57 -0500

    tarea-clase7

---
+++ js/main.js
@@ -0,0 +1,61 @@
+let num1 = 18;
+let num2 = 8;
+
+let suma = num1 + num2;
+console.log(suma);
+
+let resta = num1 - num2;
+console.log(resta);
+
+let resta2 = num2 - num1;
+console.log(resta2);
+
+let multiplicar = num1 * num2;
+console.log(multiplicar);
+
+let division = num1 / num2;
+console.log(division);
+
+let n2 = 74;
+let n3 = 14;
+
+let dividir = n2 / n3;
+console.log(dividir);
+
+let resto = n2 % n3;
+console.log(resto);
+
+let sumita = n2 + n3;
+console.log(sumita);
+
+let combinada = num1 + n3;
+console.log(combinada);
+
+let producto = num2 * n2;
+console.log(producto);
+
+let sust = n2 - num1;
+console.log(sust);
+
+
+let saludo = "Buen día, que tal";
+let pregunta = "¿Cómo estuvo tu día hoy?";
+let nombre = "Amiga Sonia";
+
+let concatenar = saludo + " " + pregunta + " " + nombre;
+console.log(concatenar);
+
+let referencia = "¡Aquí estamos!";
+let question = "¿Vienen ustedes para acá?";
+let referencia2 = "¡Estaremos esperándolos!";
+let agradecimiento = "¡Gracias por la información!";
+
+let concatenar2 = referencia + " " + question + " " + referencia2 + " " + agradecimiento;
+console.log(concatenar2); 
+
+let ir = "¡Vamos a clases!";
+let pregunta2 = "¿Cuándo?";
+let respuesta = "¡Esta noche, la clase 8!";
+
+let unit = ir + " " + pregunta2 + " " + respuesta;
+console.log(unit);
